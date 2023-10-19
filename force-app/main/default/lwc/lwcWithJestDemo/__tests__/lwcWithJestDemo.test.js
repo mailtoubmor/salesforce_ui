3d76b475d05ebcee08f0be4306eb4c8167851d25
mailtoubmor <mailstoubmor@gmail.com>
2023-10-19 07:04:56 +0530

    Update

--- force-app/main/default/lwc/lwcWithJestDemo/__tests__/lwcWithJestDemo.test.js
+++ force-app/main/default/lwc/lwcWithJestDemo/__tests__/lwcWithJestDemo.test.js
@@ -1,5 +1,11 @@
 import { createElement } from 'lwc';
 import LwcWithJestDemo from '../lwcWithJestDemo';
+
+async function flushPromises() {
+    return Promise.resolve();
+}
+
+
 describe('running jest in local for the first time',()=>{
    beforeEach(()=>{
         const component = createElement('c-lwc-with-jest-demo',{
@@ -13,4 +19,22 @@ describe('running jest in local for the first time',()=>{
         const para = jestDemo.shadowRoot.querySelector('p');
         expect(para.textContent).toBe('Hello JEST - Day One');
    });
+
+   test('onlcick to show para',()=>{
+    const jestDemo = document.querySelector('c-lwc-with-jest-demo');
+    const newPara = jestDemo.shadowRoot.querySelector('.newPara')
+    expect(newPara.textContent).toBe('Hello JEST - Day One');
+   })
+
+   test('To Test Button Click',()=>{
+    const jestDemoNew = document.querySelector('c-lwc-with-jest-demo');
+    const btn = jestDemoNew.shadowRoot.querySelectorAll('lightning-button');
+    console.log(btn);
+    expect(btn.length).toBe(1)
+    btn[0].dispatchEvent(new CustomEvent('click'));
+    return Promise.resolve().then(() => {
+        const newPara = jestDemoNew.shadowRoot.querySelector('.newPara')
+        expect(newPara.textContent).toBe('data changed');
+    });
+   })
 })
--- force-app/main/default/lwc/lwcWithJestDemo/lwcWithJestDemo.js
+++ force-app/main/default/lwc/lwcWithJestDemo/lwcWithJestDemo.js
@@ -2,4 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class LwcWithJestDemo extends LightningElement {
     message = 'Hello JEST - Day One'
+    showPara = false ;
+    handleBtnClick(event){
+        this.showPara = true ;
+        this.message = 'data changed';
+    }
 }
