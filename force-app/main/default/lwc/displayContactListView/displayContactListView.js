27e64506019d42c4209c21b6c7b9606c91e54517
mailtoubmor <mailstoubmor@gmail.com>
2023-11-19 11:54:49 +0530

    Update

---
+++ force-app/main/default/lwc/displayContactListView/displayContactListView.js
@@ -0,0 +1,37 @@
+import { LightningElement,wire } from 'lwc';
+import getContactList from '@salesforce/apex/ContactListForLwcUI.FetchContactList';
+const columns = [
+    {   
+        label: 'First Name', 
+        fieldName: 'FirstName', 
+        type: 'text' 
+    },
+    {
+        label: 'Last Name',
+        fieldName: 'LastName',
+        type: 'text'
+    },
+    {
+        label: 'Email',
+        fieldName: 'Email',
+        type: 'text'
+    },
+    {
+        label: 'Phone',
+        fieldName: 'Phone',
+        type: 'text'
+    }
+]
+export default class DisplayContactListView extends LightningElement {
+    contactList ;
+    @wire(getContactList)
+    contacts({data,error}){
+        if(data){
+            this.contactList = data;
+            console.log('DATA'+JSON.stringify(data))
+        }
+        else if(error){
+            console.log('ERROR'+JSON.stringify(error))
+        }
+    }
+}
